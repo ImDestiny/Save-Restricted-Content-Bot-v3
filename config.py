3e40bc14f4972ed468e598ca53ad18440265f2ae
ImDestiny <89016609+ImDestiny@users.noreply.github.com>
2025-04-08 08:30:41 +0530

    Update config.py

--- config.py
+++ config.py
@@ -7,13 +7,13 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-API_ID = os.getenv("API_ID", "")
-API_HASH = os.getenv("API_HASH", "")
-BOT_TOKEN = os.getenv("BOT_TOKEN", "")
-MONGO_DB = os.getenv("MONGO_DB", "")
-OWNER_ID = list(map(int, os.getenv("OWNER_ID", "").split())) # list seperated via space
+API_ID = os.getenv("API_ID", "27866551")
+API_HASH = os.getenv("API_HASH", "76057a79a74262e29d6de1e9f41aab0d")
+BOT_TOKEN = os.getenv("BOT_TOKEN", "7109878113:AAH_w4p2Kdk95dgrutucTdyKJsJxaQSrC54")
+MONGO_DB = os.getenv("MONGO_DB", "mongodb+srv://justsagara:HO51oF0IOvrZV8NB@cluster0.iy1cv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+OWNER_ID = list(map(int, os.getenv("OWNER_ID", "5437233066").split())) # list seperated via space
 DB_NAME = os.getenv("DB_NAME", "telegram_downloader")
 STRING = os.getenv("STRING", None) # optional
-LOG_GROUP = int(os.getenv("LOG_GROUP", "")) # optional with -100
+LOG_GROUP = int(os.getenv("LOG_GROUP", "-1002693852663")) # optional with -100
 MASTER_KEY = os.getenv("MASTER_KEY", "gK8HzLfT9QpViJcYeB5wRa3DmN7P2xUq") # for session encryption
 IV_KEY = os.getenv("IV_KEY", "s7Yx5CpVmE3F") # for decryption
